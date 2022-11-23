0240b2c9f8b1e1aa2fb34c8ff16accd3dc625461
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-11-23 18:51:02 +0100

    validate

--- controllers/transactions/getAll.js
+++ controllers/transactions/getAll.js
@@ -4,7 +4,10 @@ const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 5 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Transaction.find({ owner }, "-createdAt -updatedAt", { skip, limit })
+  const result = await Transaction.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  })
     .sort({ createdAt: -1 })
     .populate("owner", "email name");
   res.status(200).json(result);
